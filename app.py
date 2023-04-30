2cd7860b455f0cbcf262e9e93def3e3b9ad31021
Shivanshu24-ds <shivanshupandey.ds@gmail.com>
2023-04-30 12:12:06 +0530

    commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask.logging import create_logger
+import logging
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+LOG.info(f"Entered the Process ")
+
+@app.route('/')
+
+def home():
+    LOG.info(f"Entered the Home Function")
+    html = f"<h3>Hello World, my name is Shivanshu Pandey</h3>"
+    return html.format(format)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
